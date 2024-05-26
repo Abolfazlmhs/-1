a2a2965906aab8779b2f3820c149b2ce512a1ea2
Abolfazlmhs <161168192+Abolfazlmhs@users.noreply.github.com>
2024-05-26 20:28:04 +0330

    Add files via upload

---
+++ Program 2.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Abolfazl_Mohseni
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("the number 2 times the entered number equals to " + Tavan(number));
+
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
+            
+
+        }
+        public static int Tavan(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2 * Tavan(number - 1);
+            }
+        }
+    }
+}
+
